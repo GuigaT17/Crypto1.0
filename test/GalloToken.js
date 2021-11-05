814532e4a59d98a5dc3f4b8c3b20a3cf8db918fb
GuigaT17 <guigateixeira@protonmail.com>
2021-11-05 18:26:48 -0300

    event emitter

--- test/GalloToken.js
+++ test/GalloToken.js
@@ -29,4 +29,26 @@ contract('GalloToken', function(accounts) {
             assert.equal(adminBalance.toNumber(), 56624, 'it allocates the initial supply to admin account');
         });
     });
+
+    it('transfers tokens', function(){
+        return GalloToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 1000000);
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+            return tokenInstance.transfer(accounts[1], 250, {from: accounts[0]});
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transfered from');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transfered to');
+            assert.equal(receipt.logs[0].args._value, 250, 'logs the transfer amount');
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 250, 'adds the amount to receiving account');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 56374, 'deducts the amount from sending account');
+        });
+    });
 })
