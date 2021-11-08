da3e7d31084e78a36d1b80689caac1922f2f3d53
GuigaT17 <guigateixeira@protonmail.com>
2021-11-08 15:30:58 -0300

    approve, Approval and allowance implemented

--- contracts/GalloToken.sol
+++ contracts/GalloToken.sol
@@ -13,7 +13,14 @@ contract GalloToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -28,6 +35,12 @@ contract GalloToken {
 
         emit Transfer(msg.sender, _to, _value);
 
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 }
